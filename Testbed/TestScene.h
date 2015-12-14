34bf444d556dc43a5e71a7f50f0db746764fe90b
Pavan Jakhu <PavanJakhu@gmail.com>
2015-12-13 22:13:27 -0500

    Parented human fighter ship to camera
    - Whenever you rotate the camera the fighter ship flickers in place.
    Don't know how to fix that yet.
    - Found out that we were entering the angle and axis for a quaternion
    wrong. Have to use glm::angleAxis method instead of using the
    constructor.

--- Testbed/TestScene.h
+++ Testbed/TestScene.h
@@ -27,9 +27,17 @@ public:
 		Material sunTex("sunTexture", 0.5f, 4, Texture("sun.jpg"));
 		Material moonTex("moonTexture", 0.5f, 4, Texture("moon.jpg"));
 
-		// The human fighter ship
-		addToRoot((new GameObject(glm::vec3(0.0f, -5.0f, -80.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(4.0f)))
-			->addGameComponent(new MeshRenderer(Mesh("HumanFighter_Final.obj", 0.1f), Material("human_ship"))));
+		// The human fighter ship and camera
+		GameObject* camera = 
+			(new GameObject)
+			->addGameComponent(new CameraComponent(glm::perspective(ToRadians(75.0f), window.getAspectRatio(), 0.1f, 1000.0f)))
+			->addGameComponent(new FreeLook(window.getCenter()))
+			->addGameComponent(new FreeMove(50.0f));
+		GameObject* fighterShip = 
+			(new GameObject(glm::vec3(-2.0f, -4.0f, -10.0f), glm::quat(glm::angleAxis(glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f)))))
+			->addGameComponent(new MeshRenderer(Mesh("HumanFighter_Final.obj", 0.1f), Material("human_ship")));
+		camera->addChild(fighterShip);
+		addToRoot(camera);
 
 		// the alien fighter ship
 		addToRoot((new GameObject(glm::vec3(0.0f, -5.0f, 80.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(4.0f)))
@@ -51,13 +59,6 @@ public:
 		// The Sun
 		addToRoot((new GameObject(glm::vec3(0.0f, -5.0f, -55000.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(1000.0f)))
 			->addGameComponent(new MeshRenderer(Mesh("sphere.obj", 0.1f), Material("sunTexture"))));
-
-		addToRoot((new GameObject)
-			->addGameComponent(new CameraComponent(glm::perspective(ToRadians(75.0f), window.getAspectRatio(), 0.1f, 1000.0f)))
-			->addGameComponent(new FreeLook(window.getCenter()))
-			->addGameComponent(new FreeMove())
-			/*->addChild(new GameObject(glm::vec3(0.0f, -5.0f, 80.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(4000.0f)))
-			->addGameComponent(new MeshRenderer(Mesh("sphere2.obj", 0.1f), Material("skySphereTexture")))*/);
 
 		addToRoot((new GameObject(glm::vec3(0.0f), glm::quat(ToRadians(45.0f), glm::vec3(1, 0, 0))))
 			->addGameComponent(new DirectionalLight(glm::vec3(1.0f), 0.02f, 7, 8.0f, 1.0f)));
