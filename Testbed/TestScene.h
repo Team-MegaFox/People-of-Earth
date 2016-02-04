4304b3079497f368b3e22da51f48deccf18a1445
n00998611 <jesse.a.derochie@gmail.com>
2016-02-03 19:59:23 -0500

    Stream Works needs more work though
    Streams work just not how we expected them to work... vague I know but I
    am still testing Milestone reached!!

--- Testbed/TestScene.h
+++ Testbed/TestScene.h
@@ -24,21 +24,33 @@ public:
 		Material sunTex("sunTexture", 0.5f, 4, Texture("sun.jpg"));
 		Material moonTex("moonTexture", 0.5f, 4, Texture("moon.jpg"));
 
+		// AudioListener
+		AudioListener bill;
+		AudioSource * theListener = new AudioSource(bill);
+
+		// Stream of Music Passed through a AudioSource and into the AudioEngine
+		Stream bob("./Assets/Music/music.mp3");
+		AudioSource * ambientSounds = new AudioSource(bob);
+
 		// The human fighter ship and camera
 		GameObject* camera = 
 			(new GameObject)
 			->addGameComponent(new CameraComponent(glm::perspective(glm::radians(75.0f), window.getAspectRatio(), 0.1f, 1000.0f)))
 			->addGameComponent(new FreeLook(window.getCenter()))
-			->addGameComponent(new FreeMove(50.0f));
+			->addGameComponent(new FreeMove(50.0f))
+			->addGameComponent(theListener);
 		GameObject* fighterShip = 
 			(new GameObject(glm::vec3(-2.0f, -4.0f, -10.0f), glm::quat(glm::angleAxis(glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f)))))
 			->addGameComponent(new MeshRenderer(Mesh("HumanFighter_Final.obj", 0.1f), Material("human_ship")));
+
+		theListener->setAsListener();
 		camera->addChild(fighterShip);
 		addToRoot(camera);
 
 		// the alien fighter ship
 		addToRoot((new GameObject(glm::vec3(0.0f, -5.0f, 80.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(4.0f)))
-			->addGameComponent(new MeshRenderer(Mesh("AlienFighter_FINAL.obj", 0.1f), Material("alien_ship"))));
+			->addGameComponent(new MeshRenderer(Mesh("AlienFighter_FINAL.obj", 0.1f), Material("alien_ship")))
+			->addGameComponent(ambientSounds));
 
 		// the second human fighter ship
 		addToRoot((new GameObject(glm::vec3(0.0f, 15.0f, 80.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(4.0f)))
@@ -49,16 +61,10 @@ public:
 			->addGameComponent(new MeshRenderer(Mesh("sphere2.obj", 0.1f), Material("skySphereTexture")))
 			->addGameComponent(new FreeMove()));
 
-		// AudioListener
-		AudioListener bill;
-		AudioSource * theListener = new AudioSource(bill);
-
 		// The Earth
 		addToRoot((new GameObject(glm::vec3(0.0f, -5.0f, 550.0f), glm::angleAxis(glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f)), glm::vec3(400.0f)))
 			->addGameComponent(new MeshRenderer(Mesh("sphere.obj", 0.1f), Material("earthTexture")))
-			->addGameComponent(new PlanetSpin)
-			->addGameComponent(theListener));
-		theListener->setAsListener();
+			->addGameComponent(new PlanetSpin));
 
 		addToRoot((new GameObject(glm::vec3(0.0f, -5.0f, 4550.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(150.0f)))
 			->addGameComponent(new MeshRenderer(Mesh("sphere.obj", 0.1f), Material("moonTexture"))));
@@ -67,15 +73,21 @@ public:
 		addToRoot((new GameObject(glm::vec3(0.0f, -5.0f, -55000.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(1000.0f)))
 			->addGameComponent(new MeshRenderer(Mesh("sphere.obj", 0.1f), Material("sunTexture"))));
 
-		// Stream of Music Passed through a AudioSource and into the AudioEngine
-		Stream bob("./Assets/Music/music.mp3");
-		AudioSource * ambientSounds = new AudioSource(bob);
+
 
 		addToRoot((new GameObject(glm::vec3(0.0f), glm::quat(glm::angleAxis(glm::radians(45.0f), glm::vec3(1, 0, 0)))))
-			->addGameComponent(new DirectionalLight(glm::vec3(1.0f), 0.02f, 7, 8.0f, 1.0f))
-			->addGameComponent(ambientSounds));
+			->addGameComponent(new DirectionalLight(glm::vec3(1.0f), 0.02f, 7, 8.0f, 1.0f)));
+		
+		bob.setStream("./Assets/Music/music.mp3");
+		bob.setStream3DMinMaxDistance(1.0f);
+		bob.setStreamPosVel(glm::vec3(0.0f, -5.0f, 80.0f));
+		bob.playStream(true);
 
-		ambientSounds->playSound();
+
+		if (bob.isStreamPlaying())
+		{
+			printf("yup");
+		}
 
 		CameraComponent* cc = camera->getComponent<CameraComponent>();
 		if (cc != nullptr)
