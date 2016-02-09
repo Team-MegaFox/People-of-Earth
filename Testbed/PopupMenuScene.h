a7671d1fa63d7054a6e72502fff66e64b05474ca
Pavan Jakhu <PavanJakhu@gmail.com>
2016-02-08 21:52:38 -0500

    More testing

--- Testbed/PopupMenuScene.h
+++ Testbed/PopupMenuScene.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <MegaEngine.h>
+#include "PopMenu2Scene.h"
 
 class PopupMenuScene : public Scene
 {
@@ -14,11 +15,19 @@ public:
 			->addChild((new GameObject("Stuff in Container"))
 			->addGUIComponent(new GUILabel(glm::vec4(0.1f, 0.1f, 0.5f, 0.05f), glm::vec4(0.0f), "pop up text"))
 			->addGUIComponent(new GUIButton(glm::vec4(0.1f, 0.4f, 0.5f, 0.1f), glm::vec4(0.0f), "pop up button",
-			std::bind(&PopupMenuScene::onButtonClick, this, std::placeholders::_1)))));
+			std::bind(&PopupMenuScene::onButtonClick, this, std::placeholders::_1)))
+			->addGUIComponent(new GUIButton(glm::vec4(0.1f, 0.6f, 0.5f, 0.1f), glm::vec4(0.0f), "Exit",
+			std::bind(&PopupMenuScene::onQuitClick, this, std::placeholders::_1)))));
 	}
 
 private:
 	bool onButtonClick(const GameObject& obj)
+	{
+		getCoreEngine()->getSceneManager()->push(new PopupMenu2Scene, Modality::Popup);
+		return true;
+	}
+
+	bool onQuitClick(const GameObject& obj)
 	{
 		getCoreEngine()->getSceneManager()->pop();
 		return true;
