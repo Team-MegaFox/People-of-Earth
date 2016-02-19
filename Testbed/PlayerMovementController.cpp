33446bde9bb82b375441ee5de1886b39e9219a2e
Jesse Derochie <jesse.a.derochie@gmail.com>
2016-02-19 14:18:22 -0500

    Added Pre-Processor Macros
    Pre-processor Macros to help with code reuse within
    PlayerMovementController

--- Testbed/PlayerMovementController.cpp
+++ Testbed/PlayerMovementController.cpp
@@ -1,5 +1,13 @@
 #include "PlayerMovementController.h"
-
+#define COS_ANGLE(x) glm::cos(0.5f * x * 3.14159625f / 180.0f)
+#define SIN_ANGLE(x) glm::sin(0.5f * x * 3.14159265f / 180.0f)
+#define ROTATE_X_AXIS(x) glm::quat(COS_ANGLE(x), 0, SIN_ANGLE(x), 0)
+#define ROTATE_Y_AXIS(x) glm::quat(COS_ANGLE(x), SIN_ANGLE(x), 0, 0)
+#define SHIP_ROTATION(x, y)	glm::quat(						\
+		glm::cos(0.5f * (x + y) * 3.14159625f / 180.0f),	\
+		glm::sin(0.5f * x * 3.14159265f / 180.0f),			\
+		glm::sin(0.5f * y * 3.14159265f / 180.0f), 0		\
+)		
 
 PlayerMovementController::PlayerMovementController(float accelerationValue, int forwardKey, int backKey, int leftKey, int rightKey, int upKey, int downKey)
 {
@@ -47,14 +55,10 @@ void PlayerMovementController::lookAround(const InputManager& input)
 	if (input.GetThumbRPosition().x > 0.3f)
 	{
 		returnToActualRotation();
-		m_rigidBody->setRotation(m_rigidBody->getRotation() *
-			glm::quat(glm::cos(0.5f * -1.0f * 3.14159625f / 180.0f),
-			0, glm::sin(0.5f * -1.0f * 3.14159265f / 180.0f), 0));
+		m_rigidBody->setRotation(m_rigidBody->getRotation() * ROTATE_X_AXIS(-1.0f));
 
 		//Rotates the camera view
-		m_camera->getTransform()->setRotation(*m_camera->getTransform()->getRotation() *
-			glm::quat(glm::cos(0.5f * -1.0f * 3.14159625f / 180.0f),
-			0, glm::sin(0.5f * -1.0f * 3.14159265f / 180.0f), 0));
+		m_camera->getTransform()->setRotation(*m_camera->getTransform()->getRotation() * ROTATE_X_AXIS(-1.0f));
 		if (m_shipsVisualRotation.y > -15.0f)
 		{
 			m_shipsVisualRotation.y--;
@@ -66,14 +70,10 @@ void PlayerMovementController::lookAround(const InputManager& input)
 	if (input.GetThumbRPosition().x < -0.3f)
 	{
 		returnToActualRotation();
-		m_rigidBody->setRotation(m_rigidBody->getRotation() *
-			glm::quat(glm::cos(0.5f * 1.0f * 3.14159625f / 180.0f),
-			0, glm::sin(0.5f * 1.0f * 3.14159265f / 180.0f), 0));
+		m_rigidBody->setRotation(m_rigidBody->getRotation() * ROTATE_Y_AXIS(1.0f));
 
 		//Rotates the camera view
-		m_camera->getTransform()->setRotation(*m_camera->getTransform()->getRotation() *
-			glm::quat(glm::cos(0.5f * 1.0f * 3.14159625f / 180.0f),
-			0, glm::sin(0.5f * 1.0f * 3.14159265f / 180.0f), 0));
+		m_camera->getTransform()->setRotation(*m_camera->getTransform()->getRotation() * ROTATE_Y_AXIS(1.0f));
 		
 		if (m_shipsVisualRotation.y < 15.0f)
 		{
@@ -87,14 +87,10 @@ void PlayerMovementController::lookAround(const InputManager& input)
 	{
 		returnToActualRotation();
 
-		m_rigidBody->setRotation(m_rigidBody->getRotation() *
-			glm::quat(glm::cos(0.5f * 1.0f * 3.14159625f / 180.0f),
-			glm::sin(0.5f * 1.0f * 3.14159265f / 180.0f), 0, 0));
+		m_rigidBody->setRotation(m_rigidBody->getRotation() * ROTATE_Y_AXIS(1.0f));
 
 		//Rotates the camera view
-		m_camera->getTransform()->setRotation(*m_camera->getTransform()->getRotation() *
-			glm::quat(glm::cos(0.5f * 1.0f * 3.14159625f / 180.0f),
-			glm::sin(0.5f * 1.0f * 3.14159265f / 180.0f), 0, 0));
+		m_camera->getTransform()->setRotation(*m_camera->getTransform()->getRotation() * ROTATE_Y_AXIS(1.0f));
 		
 		if (m_shipsVisualRotation.x > -15.0f)
 		{
@@ -108,14 +104,10 @@ void PlayerMovementController::lookAround(const InputManager& input)
 	{
 		returnToActualRotation();
 
-		m_rigidBody->setRotation(m_rigidBody->getRotation() *
-			glm::quat(glm::cos(0.5f * -1.0f * 3.14159625f / 180.0f),
-			glm::sin(0.5f * -1.0f * 3.14159265f / 180.0f), 0, 0));
+		m_rigidBody->setRotation(m_rigidBody->getRotation() * ROTATE_X_AXIS(-1.0f));
 
 		//Rotates the camera view
-		m_camera->getTransform()->setRotation(*m_camera->getTransform()->getRotation() *
-			glm::quat(glm::cos(0.5f * -1.0f * 3.14159625f / 180.0f),
-			glm::sin(0.5f * -1.0f * 3.14159265f / 180.0f), 0, 0));
+		m_camera->getTransform()->setRotation(*m_camera->getTransform()->getRotation() * ROTATE_X_AXIS(-1.0f));
 
 		if (m_shipsVisualRotation.x < 15.0f)
 		{
@@ -170,25 +162,13 @@ void PlayerMovementController::returnToActualRotation()
 {
 	if (glm::abs(m_shipsVisualRotation.x) != 0 || glm::abs(m_shipsVisualRotation.y) != 0)
 	{
-		m_rigidBody->setRotation(m_rigidBody->getRotation() *
-			glm::quat(
-			glm::cos(0.5f * -(m_shipsVisualRotation.x + m_shipsVisualRotation.y) * 3.14159625f / 180.0f),
-			glm::sin(0.5f * -m_shipsVisualRotation.x * 3.14159265f / 180.0f),
-			glm::sin(0.5f * -m_shipsVisualRotation.y * 3.14159265f / 180.0f),
-			0
-			));
+		m_rigidBody->setRotation(m_rigidBody->getRotation() * SHIP_ROTATION(-m_shipsVisualRotation.x, -m_shipsVisualRotation.y));
 	}
 }
 
 void PlayerMovementController::showVisualShipRotation()
 {
-	m_rigidBody->setRotation(m_rigidBody->getRotation() *
-		glm::quat(
-		glm::cos(0.5f * (m_shipsVisualRotation.x + m_shipsVisualRotation.y) * 3.14159625f / 180.0f),
-		glm::sin(0.5f * m_shipsVisualRotation.x * 3.14159265f / 180.0f),
-		glm::sin(0.5f * m_shipsVisualRotation.y * 3.14159265f / 180.0f),
-		0
-		));
+	m_rigidBody->setRotation(m_rigidBody->getRotation() * SHIP_ROTATION(m_shipsVisualRotation.x, m_shipsVisualRotation.y));
 }
 
 void PlayerMovementController::checkLerp(const InputManager& input)
