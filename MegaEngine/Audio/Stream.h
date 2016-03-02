0418218bec77243b8eb879ac93587594bccd670a
Jesse Derochie <jesse.a.derochie@gmail.com>
2016-03-01 19:57:17 -0500

    Audio Filter Updated to Physx
    Stream, Sound, AudioListener, AudioEngine updated to support PhysX

--- MegaEngine/Audio/Stream.h
+++ MegaEngine/Audio/Stream.h
@@ -3,7 +3,7 @@
 // Created          : 02-01-2016
 //
 // Last Modified By : Jesse Derochie
-// Last Modified On : 02-25-2016
+// Last Modified On : 03-01-2016
 // ***********************************************************************
 // <copyright file="StreamSource.h" company="Team MegaFox">
 //     Copyright (c) Team MegaFox. All rights reserved.
@@ -79,7 +79,7 @@ public:
 	/// </summary>
 	/// <param name="pos">The position.</param>
 	/// <param name="vel">The vel.</param>
-	void setStreamPosVel(glm::vec3 pos, glm::vec3 vel = glm::vec3(0.0f));
+	void setStreamPosVel(physx::PxVec3 pos, physx::PxVec3 vel = physx::PxVec3(0.0f, 0.0f, 0.0f));
 
 	/// <summary>
 	/// Pan level, from -1.0 (left) to 1.0 (right), default = 0 (center).
